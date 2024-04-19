5244a73b66a4a18cdcc9f61be20ffe5a6f5f9dfb
Felix216 <216felix.santana@gmail.com>
2024-04-19 16:48:13 -0400

    commit movimiento

--- index.js
+++ index.js
@@ -5,32 +5,42 @@ class Personaje {
         this.maxhealth = health;
         this.damage = damage;
     }
-
     estaVivo() {
         return this.health > 0;
     }
+    mover() {
 
-    attack(target) {
-        
-    }
-    
-    get_vida() {
-        return this.health;
     }
 }
 
+const jugador1 = new Personaje("Jugador", 100, 10, 120);
+const jugador2 = new Personaje("Jugador", 100, 10, 800);
+
+let jugadorX = 0;
+
+const canvas = document.getElementById("canva");
+const ctx = canvas.getContext("2d");
 
 document.addEventListener("keydown", function(event) {
     if (event.key === "a") {
-
+        jugadorX -=10
     }
     if (event.key === "d") {
-        
+        jugadorX +=10
     }
 });
 
-var c = document.getElementById("canvas");
-var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+function dibujar(posX) {
+    ctx.beginPath();
+    ctx.rect(posX, 560, 80, 160);
+    ctx.stroke();
+}
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    dibujar(jugadorX);
+}
+
+setInterval(function() {
+    update();
+}, 16);
