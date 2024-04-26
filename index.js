dea666fabf7556fbd001aa5b8f09b286a5214f53
Daniel Alejandro <d.toro05@ufromail.cl>
2024-04-26 03:02:39 -0400

    battle is work

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const canvas = document.getElementById("canva");
 ctx = canvas.getContext('2d')
 
 class Personaje {
-    constructor(name, health, damage, posX) {
+    constructor(name, health, damage, posX,sentido) {
         this.name = name;
         this.health = health;
         this.maxhealth = health;
         this.damage = damage;
         this.posX = posX;
         this.isAttacking = false;
+        this.sentido = sentido
         
         this.velocity = 0;
         this.weapon = { dimentions : {maxLen: 200, minLen: 40, height: 40, len: 40},
-        positions : {x : this.posX + 20, y : canvas.height-110}};
+        positions : {x : this.posX + 40, y : canvas.height-110}};
         
     }
     async attack(num) {
@@ -42,25 +43,30 @@ class Personaje {
         if (this.posX < 0){
             this.velocity = 0
             this.posX = 0
-            this.weapon.positions.x = this.posX + 20
+            this.weapon.positions.x = this.posX + 40
         }
         
         if(this.posX > canvas.width-80){
             this.velocity = 0
             this.posX = canvas.width-80
-            this.weapon.positions.x = this.posX + 20
+            this.weapon.positions.x = this.posX + 40
         }
         
         ctx.fillRect(this.posX, canvas.height-160, 80, 160);
         ctx.fillStyle = 'blue';
-        ctx.fillRect(this.weapon.positions.x,this.weapon.positions.y,this.weapon.dimentions.len,this.weapon.dimentions.height);
+        if(this.sentido === 1)ctx.fillRect(this.weapon.positions.x,this.weapon.positions.y,this.weapon.dimentions.len,this.weapon.dimentions.height);
+        if(this.sentido === -1)ctx.fillRect(this.weapon.positions.x,this.weapon.positions.y,-this.weapon.dimentions.len,this.weapon.dimentions.height);
+
     }
 }
 
-const jugador = new Personaje("Jugador", 100, 1,100);
-const enemigo = new Personaje("Enemigo", 100, 1,1000);
+const jugador = new Personaje("Jugador", 100, 1,100,1);
+const enemigo = new Personaje("Enemigo", 100, 1,1000,-1);
 
+
+//Ataque del jugador
 document.addEventListener("keydown", event => {
+    
     if(event.key == "e") jugador.isAttacking = true   
 })
 
@@ -68,6 +74,8 @@ document.addEventListener("keyup", event => {
     if(event.key == "e") jugador.isAttacking = false   
 })
 
+
+//Ataque del enemigo
 document.addEventListener("keydown", event => {
     if(event.key == "m") enemigo.isAttacking = true    
 })
@@ -76,16 +84,20 @@ document.addEventListener("keyup", event => {
     if(event.key == "m") enemigo.isAttacking = false  
 })
 
+
+//Movimiento del jugador
 document.addEventListener("keypress", event => {
     if (event.key == "a") jugador.velocity = -5
     if (event.key == "d") jugador.velocity = 5
 });
+
 
 document.addEventListener("keyup", event => {
     if (event.key == "a") jugador.velocity = 0
     if (event.key == "d") jugador.velocity = 0
 });
 
+//Movimiento del enemigo
 document.addEventListener("keydown", event => {   
     if (event.key == "ArrowLeft") enemigo.velocity = -5
     if (event.key == "ArrowRight") enemigo.velocity = 5
@@ -101,7 +113,6 @@ function comprobarColision(atacante, objetivo) {
     const anchoArma = posicionArma + atacante.weapon.dimentions.len;
     const objetivoPosicion = objetivo.posX;
     const anchoObjetivo = objetivoPosicion + 80;
-
     return anchoArma >= objetivoPosicion && posicionArma <= anchoObjetivo;
 }
 
@@ -115,14 +126,18 @@ function dibujar() {
 
 function update() {
     dibujar();
-
-    if (comprobarColision(jugador, enemigo)) {
-        enemigo.health -= jugador.damage;
-        document.querySelector('#enemigoVida').style.width = enemigo.health + '%';
-    }
-    if (comprobarColision(enemigo, jugador)) {
+  
+    let posicionArmaEnemiga = enemigo.posX-enemigo.weapon.dimentions.len;
+    let posicionArmaJugador = jugador.weapon.positions.x + jugador.weapon.dimentions.len
+    if(posicionArmaEnemiga < jugador.posX+20) {
+        console.log("enemigo ataca a jugador")
         jugador.health -= enemigo.damage;
         document.querySelector('#jugadorVida').style.width = jugador.health + '%';
+    }
+    if(posicionArmaJugador > enemigo.posX){
+        console.log("Jugador ataca a enemigo")
+        enemigo.health -= jugador.damage;
+        document.querySelector('#enemigoVida').style.width = enemigo.health + '%';
     }
     
     window.requestAnimationFrame(update);
--- main.js
+++ main.js
@@ -1,41 +1,47 @@
 const canvas = document.getElementById("canva");
 ctx = canvas.getContext('2d')
 
+
 class Personaje {
-    constructor(name, health, damage, posX) {
+    constructor(name, health, damage, posX, sentido) {
         this.name = name;
         this.health = health;
         this.maxhealth = health;
         this.damage = damage;
         this.posX = posX;
         this.isAttacking = false;
+        this.sentido = sentido;
+        this.width = 80;
         
         this.velocity = 0;
         this.weapon = { dimentions : {maxLen: 200, minLen: 40, height: 40, len: 40},
-        positions : {x : this.posX + 20, y : canvas.height-110}};
+        positions : {x : this.posX+40, y : canvas.height-110}};
         
     }
 
-    async attack(num) {
+     attack(num) {
        
 
-       switch(this.isAttacking){
-        case true:
-            if(this.weapon.dimentions.len >= this.weapon.dimentions.maxLen){
-                this.isAttacking = false;
-
-            }else{
- 
-                this.weapon.dimentions.len += num;
-            }
-
-            break;
-        case false:
-            if(this.weapon.dimentions.len > this.weapon.dimentions.minLen){
-                this.weapon.dimentions.len -= num;
-            }
-            break
-       }
+           switch(this.isAttacking){
+            
+            case true:
+                if(this.weapon.dimentions.len >= this.weapon.dimentions.maxLen){
+                    this.isAttacking = false;
+    
+                }else{
+    
+                    this.weapon.dimentions.len += num;
+                }
+    
+                break;
+            case false:
+                if(this.weapon.dimentions.len > this.weapon.dimentions.minLen){
+                    this.weapon.dimentions.len -= num;
+                }
+                break
+           }
+       
+    
 
 
         
@@ -53,18 +59,20 @@ class Personaje {
         if (this.posX < 0){
             this.velocity = 0
             this.posX = 0
-            this.weapon.positions.x = this.posX + 20
+            this.weapon.positions.x = this.posX+40
         }
         
         if(this.posX > canvas.width-80){
             this.velocity = 0
             this.posX = canvas.width-80
-            this.weapon.positions.x = this.posX + 20
+            this.weapon.positions.x = this.posX
         }
         
-        ctx.fillRect(this.posX, canvas.height-160, 80, 160);
+        ctx.fillRect(this.posX, canvas.height-160, this.width, 160);
         ctx.fillStyle = 'blue';
-        ctx.fillRect(this.weapon.positions.x,this.weapon.positions.y,this.weapon.dimentions.len,this.weapon.dimentions.height)
+        if(this.sentido === 1) ctx.fillRect(this.weapon.positions.x,this.weapon.positions.y,this.weapon.dimentions.len,this.weapon.dimentions.height)
+        if(this.sentido === -1) ctx.fillRect(this.weapon.positions.x,this.weapon.positions.y,-this.weapon.dimentions.len,this.weapon.dimentions.height)
+
 
         
     }
@@ -73,8 +81,8 @@ class Personaje {
 }
 
 
-const jugador = new Personaje("Jugador", 100, 120,100);
-const enemigo = new Personaje("Jugador", 100, 600,1000);
+const jugador = new Personaje("Jugador", 100, 120,100,1);
+const enemigo = new Personaje("Jugador", 100, 600,230,-1);
 
 
 
@@ -127,19 +135,37 @@ document.addEventListener("keyup", event => {
 });
 
 
+function gestColision(){
+    
+}
 
 function dibujar() {
+    
     canvas.width = canvas.width;
     ctx.fillStyle = 'black';
     jugador.update();
     ctx.fillStyle = 'red';
-    enemigo.update();
+    enemigo.update();  
+    
 
 }
 
 function update() {
     dibujar();
+    let posicionArmaEnemiga = enemigo.posX-enemigo.weapon.dimentions.len;
+    let posicionArmaJugador = jugador.weapon.positions.x + jugador.weapon.dimentions.len
+    if(posicionArmaEnemiga < jugador.posX+20) {
+        console.log("enemigo ataca a jugador")
+        jugador.health -= enemigo.damage;
+        document.querySelector('#jugadorVida').style.width = jugador.health + '%';
+    }
+    if(posicionArmaJugador > enemigo.posX){
+        console.log("Jugador ataca a enemigo")
+        enemigo.health -= jugador.damage;
+        document.querySelector('#enemigoVida').style.width = enemigo.health + '%';
+    }
     window.requestAnimationFrame(update);
 }
+
 
 update();
